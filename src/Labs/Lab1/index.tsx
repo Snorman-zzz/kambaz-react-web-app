c68a43609c9d4f4e632b162ebb567dd3d2ae3316
Norman Yuan <normanyuan4@gmail.com>
2025-06-30 19:14:14 -0700

    Got rid of extra code

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -27,19 +27,6 @@ export default function Lab1() {
                 <h4>List Tags</h4>
                 <h5>Ordered List Tag</h5>
                 How to make pancakes:
-                1. Mix dry ingredients.
-                2. Add wet ingredients.
-                3. Stir to combine.
-                4. Heat a skillet or griddle.
-                5. Pour batter onto the skillet.
-                6. Cook until bubbly on top.
-                7. Flip and cook the other side.
-                8. Serve and enjoy!
-            </div>
-            <div id="wd-lists">
-                <h4>List Tags</h4>
-                <h5>Ordered List Tag</h5>
-                How to make pancakes:
                 <ol id="wd-pancakes">
                     <li>Mix dry ingredients.</li>
                     <li>Add wet ingredients.</li>
