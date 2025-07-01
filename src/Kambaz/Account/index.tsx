f8d4b949ec494915eb99e5ddc64772c73a614b19
Norman Yuan <normanyuan4@gmail.com>
2025-06-30 19:05:36 -0700

    got rid of duplicates

--- src/Kambaz/Account/index.tsx
+++ src/Kambaz/Account/index.tsx
@@ -2,15 +2,11 @@ import Signin from "./Signin";
 import {Navigate, Route, Routes} from "react-router";
 import Profile from "./Profile.tsx";
 import Signup from "./Signup.tsx";
-import AccountNavigation from "../Navigation.tsx";
 export default function Account() {
     return (
         <div id="wd-account-screen">
             <table>
                 <tr>
-                    <td valign="top">
-                        <AccountNavigation/>
-                    </td>
                     <td valign="top">
                         <Routes>
                             <Route path="/" element={<Navigate to="/Kambaz/Account/Signin"/>}/>
