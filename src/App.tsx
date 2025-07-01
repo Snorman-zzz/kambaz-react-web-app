57082f0027ca5122c7419901374e2a0f34154ed4
Norman Yuan <normanyuan4@gmail.com>
2025-06-30 17:48:27 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Labs from "./Labs";
+import {HashRouter, Navigate, Route, Routes} from "react-router";
+import Kambaz from "./Kambaz";
+
+function App() {
+  return (
+      <HashRouter>
+          <div>
+              <Routes>
+                  <Route path="/" element={<Navigate to="Labs" />} />
+                  <Route path="/Labs/*" element={<Labs />} />
+                  <Route path="/Kambaz/*" element={<Kambaz />} />
+              </Routes>
+          </div>
+      </HashRouter>
+  )
+}
+
+export default App
---
+++ src/Labs/index.tsx
@@ -0,0 +1,24 @@
+import Lab1 from "./Lab1"
+import {Navigate, Route, Routes} from "react-router";
+import Lab2 from "./Lab2";
+import Lab3 from "./Lab3";
+
+function Index() {
+    return null;
+}
+
+export default function Labs() {
+    return (
+        <div id="wd-labs">
+            <h1>Labs</h1>
+            <Index />
+            <Routes>
+                <Route path="/" element={<Navigate to="Lab1" />} />
+                <Route path="Lab1" element={<Lab1 />} />
+                <Route path="Lab2/*" element={<Lab2 />} />
+                <Route path="Lab3/*" element={<Lab3 />} />
+            </Routes>
+
+        </div>
+    );
+}
